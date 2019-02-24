dac615b7b9a7f3aa221026181521270567e5ff69
Brian Zhu <bzhu75@gatech.edu>
2019-02-24 14:53:32 -0500

    second commit of solar system

--- app/src/main/java/com/example/tejasvedantham/spacetrader/model/Resource.java
+++ app/src/main/java/com/example/tejasvedantham/spacetrader/model/Resource.java
@@ -1,4 +1,17 @@
 package com.example.tejasvedantham.spacetrader.model;
 
 public enum Resource {
+    NOSPECIALRESOURCES,
+    MINERALRICH,
+    MINERALPOOR,
+    DESERT,
+    LOTSOFWATER,
+    RICHSOIL,
+    POORSOIL,
+    RICHFAUNA,
+    LIFELESS,
+    WEIRDMUSHROOMS,
+    LOTSOFHERBS,
+    ARTISTIC,
+    WARLIKE;
 }
--- app/src/main/java/com/example/tejasvedantham/spacetrader/model/TechLevel.java
+++ app/src/main/java/com/example/tejasvedantham/spacetrader/model/TechLevel.java
@@ -1,4 +1,12 @@
 package com.example.tejasvedantham.spacetrader.model;
 
 public enum TechLevel {
+    PREAGRICULTURE,
+    AGRICULTURE,
+    MEDIEVAL,
+    RENAISSANCE,
+    EARLYINDUSTRIAL,
+    INDUSTRIAL,
+    POSTINDUSTRIAL,
+    HITECH;
 }
