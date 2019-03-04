6243db057a635691d1157375b7480182a3aa0fae
Tejas Vedantham <tejas6080@gmail.com>
2019-03-04 14:06:18 -0500

    Added Market stuff

--- app/src/main/java/com/example/tejasvedantham/spacetrader/views/MainGame.java
+++ app/src/main/java/com/example/tejasvedantham/spacetrader/views/MainGame.java
@@ -1,7 +1,10 @@
 package com.example.tejasvedantham.spacetrader.views;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.example.tejasvedantham.spacetrader.R;
@@ -10,13 +13,22 @@ import org.w3c.dom.Text;
 
 public class MainGame extends AppCompatActivity {
 
-    private TextView mainText;
+    private TextView solarSystemText;
+    private TextView planetText;
+    private Button marketButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.main_game);
 
-        mainText = (TextView) findViewById(R.id.mainText);
+        solarSystemText = (TextView) findViewById(R.id.solarSystemText);
+        planetText = (TextView) findViewById(R.id.planetText);
+        marketButton = (Button) findViewById(R.id.marketButton);
+    }
+
+    public void goToMarket(View view) {
+        Intent intent = new Intent(getApplicationContext(), Market.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/tejasvedantham/spacetrader/views/Market.java
@@ -0,0 +1,25 @@
+package com.example.tejasvedantham.spacetrader.views;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import com.example.tejasvedantham.spacetrader.R;
+
+public class Market extends AppCompatActivity {
+
+    private TextView marketTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.setContentView(R.layout.market);
+
+        initialize();
+    }
+
+    private void initialize() {
+        marketTextView = (TextView) findViewById(R.id.marketText);
+    }
+
+}
